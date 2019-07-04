4d48334dac7b997c25d56fee9a3bca4ae2c13f16
anisiobfilho <41025743+anisiobfilho@users.noreply.github.com>
2019-07-04 16:14:39 -0300

    Criação do Prjeto e do Model Coordenada

---
+++ winkelmonitor/initial/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Coordenada
+
+admin.site.register(Coordenada)
---
+++ winkelmonitor/initial/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Coordenada(models.Model):
+    time = models.DateTimeField('instante', default = timezone.now())
+    pitch = models.FloatField()
+    yaw = models.FloatField()
+    roll = models.FloatField()
+    def __str__(self):
+        return str(self.time)+" Pitch:"+str(self.pitch)+" Yaw:"+str(self.yaw)+" Roll:"+str(self.roll)
+
